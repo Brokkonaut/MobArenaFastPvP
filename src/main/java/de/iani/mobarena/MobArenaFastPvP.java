34ce2cbdddf9c10270fe8f17b95abba356364d9c
Brokkonaut <hannos17@gmx.de>
2024-07-24 05:46:32 +0200

    1.21, attributes

--- src/main/java/de/iani/mobarena/MobArenaFastPvP.java
+++ src/main/java/de/iani/mobarena/MobArenaFastPvP.java
@@ -1,26 +1,24 @@
 package de.iani.mobarena;
 
-import java.util.UUID;
-
+import com.garbagemule.MobArena.ArenaPlayer;
+import com.garbagemule.MobArena.events.NewWaveEvent;
+import com.garbagemule.MobArena.framework.Arena;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.attribute.AttributeModifier.Operation;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.EquipmentSlotGroup;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.garbagemule.MobArena.ArenaPlayer;
-import com.garbagemule.MobArena.events.NewWaveEvent;
-import com.garbagemule.MobArena.framework.Arena;
-
 public class MobArenaFastPvP extends JavaPlugin implements Listener {
-    private static final UUID attackSpeedUUID = UUID.fromString("34f5963d-3fc6-474b-a576-c4f05c2af419");
-    private static final UUID attackDamageUUID = UUID.fromString("ea434c08-d745-4b5c-858e-1db76cc70088");
+    private static final NamespacedKey attackSpeedKey = NamespacedKey.fromString("mobarenafastpvp:attackspeed");
+    private static final NamespacedKey attackDamageKey = NamespacedKey.fromString("mobarenafastpvp:attackdamage");
 
     @Override
     public void onEnable() {
@@ -70,8 +68,8 @@ public class MobArenaFastPvP extends JavaPlugin implements Listener {
             if (meta.hasAttributeModifiers()) {
                 return itemStack;
             }
-            meta.addAttributeModifier(Attribute.GENERIC_ATTACK_SPEED, new AttributeModifier(attackSpeedUUID, "1.8-attackspeed", 1.5, Operation.ADD_NUMBER, EquipmentSlot.HAND));
-            meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, new AttributeModifier(attackDamageUUID, "1.8-attackdamage", addDamage, Operation.ADD_NUMBER, EquipmentSlot.HAND));
+            meta.addAttributeModifier(Attribute.GENERIC_ATTACK_SPEED, new AttributeModifier(attackSpeedKey, 1.5, Operation.ADD_NUMBER, EquipmentSlotGroup.HAND));
+            meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, new AttributeModifier(attackDamageKey, addDamage, Operation.ADD_NUMBER, EquipmentSlotGroup.HAND));
 
             itemStack.setItemMeta(meta);
         }
